8ef41ba8552349436fa0293d21127dccc1636a5f
David Luposchainsky <dluposchainsky@gmail.com>
2017-08-05 13:12:59 +0200

    Implement the test via comparison to reference files

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,32 +1,30 @@
-import Data.Aeson (Value)
-import qualified Data.Aeson as Json
+import Data.Text.Prettyprint.Doc
+import Data.Text.Prettyprint.Doc.Render.Text
 import JsonDiff
 import Protolude
+import qualified Data.Aeson as Json
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.IO as TL
 
 main :: IO ()
 main = do
-  putText $ prettyDiff $ diffStructures obj1 obj2
-
-obj1 :: Value
-obj1 =
-  (\(Just x) -> x) $
-  Json.decode
-    "[{\"serviceId\": 0, \"serviceName\": \"string\", \"requiredServiceDataItems\": [{\"profileDataFieldItem\": {\"fieldOwnerType\": \"ACCOUNT\", \"itemId\": \"USER_ID\", \"profileDataFieldRelationshipType\": \"GROUP\", \"childrenIds\": [\"USER_ID\"]}, \"profileDataFieldGroup\": {\"itemId\": \"string\", \"profileDataFieldRelationshipType\": \"GROUP\", \"childrenIds\": [\"USER_ID\"]}}], \"requiredCustomProperties\": [\"string\"]}]"
-
-obj2 :: Value
-obj2 =
-  (\(Just x) -> x) $
-  Json.decode
-    "[{\"serviceId\": 0, \"serviceName\": \"string\", \"requiredServiceDataItems\": [{\"profileDataFieldItem\": {\"fieldOwnerType\": \"ACCOUNT\", \"itemId\": \"USER_ID\", \"profileDataFieldRelationshipType\": \"GROUP\", \"childrenIds\": [\"USER_ID\", \"shouldNotBeFound\"]}, \"profileDataFieldGroup\": {\"itemId\": \"string\", \"profileDataFieldRelationshipType\": {\"test\": 15}, \"childrenIds\": [\"USER_ID\"]}}], \"requiredCustomProperties\": [15]}]"
-
-test1 :: Value
-test1 =
-  (\(Just x) -> x) $
-  Json.decode
-    "[{\"serviceId\": 0, \"serviceName\": \"string\", \"requiredServiceDataItems\": [{\"profileDataFieldItem\": {\"fieldOwnerType\": \"ACCOUNT\", \"itemId\": \"USER_ID\", \"profileDataFieldRelationshipType\": \"GROUP\", \"childrenIds\": [\"USER_ID\"]}, \"profileDataFieldGroup\": {\"itemId\": \"string\", \"profileDataFieldRelationshipType\": \"GROUP\", \"childrenIds\": [\"USER_ID\"]}}], \"requiredCustomProperties\": [\"string\"]}]"
-
-test2 :: Value
-test2 =
-  (\(Just x) -> x) $
-  Json.decode
-    " [{\"serviceId\":1,\"requiredServiceDataItems\":[{\"profileDataFieldItem\":null,\"profileDataFieldGroup\":{\"profileDataFieldRelationshipType\":\"GROUP\",\"childrenIds\":[\"IS_CONTACTED_BY_EMAIL\",\"IS_CONTACTED_BY_PHONE\",\"IS_CONTACTED_BY_LETTER\"],\"itemId\":\"COMMUNICATION_CHANNEL\"}},{\"profileDataFieldItem\":{\"profileDataFieldRelationshipType\":\"DATAFIELD\",\"childrenIds\":[\"LANDLINE_PHONE\",\"MOBILE_PHONE\"],\"fieldOwnerType\":\"ACCOUNT\",\"itemId\":\"IS_CONTACTED_BY_PHONE\"},\"profileDataFieldGroup\":null},{\"profileDataFieldItem\":{\"profileDataFieldRelationshipType\":\"DATAFIELD\",\"childrenIds\":null,\"fieldOwnerType\":\"ACCOUNT\",\"itemId\":\"USER_ID\"},\"profileDataFieldGroup\":null}],\"requiredCustomProperties\":[]}] "
+    fileA <- BSL.readFile "test/a.json"
+    fileB <- BSL.readFile "test/b.json"
+    case (Json.decode fileA, Json.decode fileB) of
+        (Just jsonA, Just jsonB) -> do
+            expected <- TL.readFile "test/expected.output"
+            let diff = diffStructures jsonA jsonB
+                renderedDiff = (renderLazy . layoutPretty defaultLayoutOptions . pretty) diff
+            case TL.strip renderedDiff == TL.strip expected of
+                False -> do
+                    putText "Error: actual and expected diff do not match."
+                    putText "Actual:"
+                    putText (TL.toStrict renderedDiff)
+                    putText "Expected:"
+                    putText (TL.toStrict expected)
+                    exitFailure
+                True -> pure ()
+        _otherwise -> do
+            putText "Uh oh, input isn’t proper JSON"
+            exitFailure
